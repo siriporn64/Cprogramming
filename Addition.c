3afffe30e3d6e0f7ccfd8da1bcb1644a0dd94f6d
siriporn64 <s6302041620141@email.kmutnb.ac.th>
2021-01-13 20:50:55 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//ดึง Library Input Output
+
+void main(){
+// ส่วนที่ 2 Function หลักของภาษา C คือ Fx Main
+
+// ---------- คำสั่งโปรแกรมบวกเลข ----------//
+    int num1, num2, total;
+    // ประกาศตัวแปร num1 num2 และ total
+    printf("Value of num1 is:");
+    scanf("%d",&num1);
+    printf("Value of num2 is:");
+    scanf("%d",&num2);
+    total = num1 + num2;
+    printf("Result is %d\n",total);
+    printf("Siriporn Noppakhun\n");
+
+
+
+// ---------- สิ้นสุดโปรแกรมบวกเลข ----------//
+
+}

9b1ebf43f133bf504291ca826c3d566ceb456b6b
siriporn64 <s6302041620141@email.kmutnb.ac.th>
2021-01-13 20:57:06 +0700

    HelloWorld

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>//  ส่วนหัวสำหรับดึง  Library (1)
+//  การกด ctrl + / คือการใส่คอมเม้นต์แบบเร็ว short cut key
+
+//  ส่วน function หลัก (2)
+int main(){
+    printf("Siriporn Noppakhun\n");
+    printf("Mew\n");
+    printf("20\n");
+    printf("Travel\n");
+    printf("Go to korea\n");
+    printf("See parents smile\n");
+    /*
+        ส่วนคำสั่งการทำงาน (3)
+        printf คือ ฟังก์ชันที่ไว้สำหรับแสดงผลทางหน้าจอ
+    */
+    //  วิธีการจัดชุดคำสั่งให้สวย ให้ shift + alt + f
+    //  การเปิด Terminal ให้กด ctrl + j
+}
